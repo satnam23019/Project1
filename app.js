c90a0640b21a1c7313c11ff38f8f804ec8e1ad6e
Satnam Singh <ssingh23019@gmail.com>
2025-07-23 23:15:07 +0530

    New Database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require ("mongoose");
 const Listing = require ("./models/listing.js");
 const path = require ("path");
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust001";
 
 main()
 .then(()=>{
