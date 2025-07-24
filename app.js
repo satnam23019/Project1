19707317c25fbf08431f4a5db47c4e7f2d043eb9
Satnam Singh <ssingh23019@gmail.com>
2025-07-24 06:40:58 +0530

    minor changes in all files

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require ("mongoose");
 const Listing = require ("./models/listing.js");
 const path = require ("path");
+const methodOverride = require("method-override");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -21,6 +22,7 @@ await mongoose.connect(MONGO_URL);
 app.set ("view engine", "ejs");
 app.set ("views", path.join (__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 
 app.get ("/", (req, res) => {
@@ -80,6 +82,13 @@ app.get("/listings/:id/edit", async (req, res) =>{
         res.render("./listings/edit.ejs", {listing});
 });
 
+
+//Update Route
+app.put("/listings/:id", async (req, res) => {
+        let {id} = req.params;
+        await Listing.findByIdAndUpdate(id, {...req.body.listing});
+        res.redirect(`/listings/${id}`);
+})
 
 app.listen(3300, ()=> {
         console.log ("server is Listening to port 3300");
